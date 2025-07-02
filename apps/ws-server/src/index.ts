0805bed17f4faf0e5ed86bb5dbaeae4686a24cce
Arav menon <aravmenon.ak@gmail.com>
2025-07-03 00:17:11 +0530

    final setup

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,8 +1,26 @@
 import { WebSocketServer } from 'ws'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import 'dotenv/config'
 
 const wss = new WebSocketServer( { port : 8080 })
 
-wss.on('connection', (socket) => {
+wss.on('connection', (socket, request) => {
+
+    const url = request.url
+
+    if(!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?') [1])
+    const token = queryParams.get('token') ?? ""
+
+    const decoded = jwt.verify(token, "process.env.JWT_ROOM_TOKEN")
+
+    if(!decoded || !(decoded as JwtPayload ).userId) {
+        wss.close()
+        return
+    }   
     socket.on('message', (mssg) => {
         console.log(`Recieve data from client ${mssg}`)
     })
