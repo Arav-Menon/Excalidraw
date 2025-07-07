cb3ab8ce6fcb4114d1282a548ca0c73131d71843
Arav menon <aravmenon.ak@gmail.com>
2025-07-07 11:01:42 +0530

    add signin zod validation

--- packages/common/src/authValidation.ts
+++ packages/common/src/authValidation.ts
@@ -13,3 +13,16 @@ export const SignupValidations = z.object({
   //     .refine((password) => !/\s/.test(password), 'Password cannot contain whitespace')
   password: z.string().trim().min(6, "Password must be at least 6 characters"),
 });
+
+export const SigninValidations = z.object({
+  email: z.string().email("Invalid email format"),
+  //  password: z.string()
+  //     .min(8, 'Password must be at least 8 characters')
+  //     .max(100, 'Password cannot exceed 100 characters')
+  //     .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  //     .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+  //     .regex(/[0-9]/, 'Password must contain at least one number')
+  //     .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character')
+  //     .refine((password) => !/\s/.test(password), 'Password cannot contain whitespace')
+  password: z.string().trim().min(6, "Password must be at least 6 characters"),
+});
