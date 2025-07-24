450779e0d0543bd71d6dd635691655bb41322be8
Arav menon <aravmenon.ak@gmail.com>
2025-07-25 04:13:24 +0530

    add ui components

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,14 +5,16 @@ import { ReactNode } from "react";
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  variant?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, variant }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      //@ts-ignore
+      variant={variant}
     >
       {children}
     </button>
